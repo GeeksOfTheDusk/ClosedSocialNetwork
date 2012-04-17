62870fd0111b6101a4f84c805cc4045461800ea3
Waldemar Artes <artes@ztt.fh-worms.de>
2012-04-17 18:57:04 +0200

    Rename App from NSA to CSN.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "NSA"
+    val appName         = "CSN"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
