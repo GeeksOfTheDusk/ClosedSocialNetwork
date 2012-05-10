50f5d486270d38ee9213afc64a4fb48a9fa71a5d
Frogurth <artes.waldi@googlemail.com>
2012-05-10 21:57:43 +0200

    Extract twitter bootstrap to a separate play 2 module.

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "csn_bbcodeparser" % "csn_bbcodeparser_2.9.1" % "1.0-SNAPSHOT"
+      "csn_bbcodeparser" % "csn_bbcodeparser_2.9.1" % "1.0-SNAPSHOT",
+      "twitter_bootstrap_module" % "twitter_bootstrap_module_2.9.1" % "1.0-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      resolvers += "CSN Modules Github Repository" at "csnmodules.github.com"
+      resolvers += Resolver.url("CSN Modules Repository", url("http://frogurth.github.com/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
