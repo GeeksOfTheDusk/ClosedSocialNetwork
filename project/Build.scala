90828d8f50b0d40426c98f917bd76857c7a53328
Frogurth <artes.waldi@googlemail.com>
2012-05-25 19:43:28 +0200

    Modified Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -5,18 +5,16 @@ import PlayProject._
 object ApplicationBuild extends Build {
 
     val appName         = "CSN"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "0.3.0"
 
     val appDependencies = Seq(
       "mysql" % "mysql-connector-java" % "5.1.20",
       "csn_bbcodeparser" % "csn_bbcodeparser_2.9.1" % "1.0-SNAPSHOT",
       "twitter_bootstrap_module" % "twitter_bootstrap_module_2.9.1" % "1.0-SNAPSHOT",
-      "csn_mini_blogengine" % "csn_mini_blogengine_2.9.1" % "1.0-SNAPSHOT"
+      "csn_mini_blogengine" % "csn_mini_blogengine_2.9.1" % "0.3.0"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       resolvers += "CSN Modules Repository" at "http://repositories.coding-minds.com/modules/releases/"
-      //resolvers += "Local Play Repository" at "file:///usr/local/Cellar/play/2.0.1/libexec/repository/local/"
     )
-
 }
